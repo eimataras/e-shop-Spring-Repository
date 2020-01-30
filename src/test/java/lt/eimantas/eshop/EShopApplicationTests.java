17a3405907709a58d69c673f9ab4987e3bccbcc8
Eimantas Tarasevičius <eimataras@gmail.com>
2020-01-30 16:19:42 +0200

    first commit

---
+++ src/test/java/lt/eimantas/eshop/EShopApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.eimantas.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
