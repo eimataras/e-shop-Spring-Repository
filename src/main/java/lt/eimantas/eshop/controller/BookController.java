9a56123c9c6fbdffb728212982afeafa5fbc90dd
Eimantas Tarasevičius <eimataras@gmail.com>
2020-02-04 15:59:23 +0200

    Set Up CRUD on Book

--- src/main/java/lt/eimantas/eshop/controller/BookController.java
+++ src/main/java/lt/eimantas/eshop/controller/BookController.java
@@ -3,26 +3,49 @@ package lt.eimantas.eshop.controller;
 import lt.eimantas.eshop.mapper.BookMapper;
 import lt.eimantas.eshop.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/book")
+@RequestMapping(value = "/book")
 public class BookController {
 
-  //  @Autowired
+    @Autowired
     private BookMapper bookMapper;
 
-    public BookController(BookMapper bookMapper) {
-        this.bookMapper = bookMapper;
-    }
 
     @GetMapping("/all")
-    public List<Book> getAll() {
+    public List<Book> getAllBooks() {
         return bookMapper.findAll();
     }
 
+    @GetMapping("/:id")
+    public @ResponseBody
+    Optional<Book> getOneBookById(@RequestParam Integer book_id) {
+        Optional<Book> result = bookMapper.findById(book_id);
+        return result;
+    }
+
+    @PostMapping("/add")
+    public void addBook(@RequestBody Book book) {
+//        Book book = new Book();
+//        book.setTitle("Pasakos vaikams");
+//        book.setAuthor("Kazys Binkis");
+//        book.setPublished_date("2010-01-01");
+//        book.setBook_cover("https://...");
+//        book.setQuantity(5);
+        bookMapper.add(book);
+    }
+
+    @PutMapping("/edit")
+    public void updateBook(@RequestBody Book book) {
+        bookMapper.update(book);
+    }
+
+    @DeleteMapping("/delete")
+    public void deleteBook(@RequestParam Integer book_id) {
+        bookMapper.deleteById(book_id);
+    }
 }
--- src/main/java/lt/eimantas/eshop/mapper/BookMapper.java
+++ src/main/java/lt/eimantas/eshop/mapper/BookMapper.java
@@ -1,15 +1,28 @@
 package lt.eimantas.eshop.mapper;
 
 import lt.eimantas.eshop.model.Book;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface BookMapper {
 
-    @Select("select * from book")
+    @Select("SELECT * FROM `Book`")
     List<Book> findAll();
+
+    @Select("SELECT * FROM `Book` WHERE `book_id`=#{book_id}")
+    Optional<Book> findById(Integer book_id);
+
+    @Insert("INSERT INTO `Book` (`title`, `author`, `published_date`, `book_cover`, `quantity`) VALUES (#{title}, #{author}, #{published_date}, #{book_cover}, #{quantity})")
+    @SelectKey(statement = "SELECT SCOPE_IDENTITY()", keyProperty = "id", before = false, resultType = Integer.class)
+    void add(Book book);
+
+    @Update("UPDATE `Book` SET `title`=#{title}, `author`=#{author}, `published_date`=#{published_date}, `book_cover`=#{book_cover}, `quantity`=#{quantity} WHERE `book_id`=#{book_id}")
+    void update(Book book);
+
+    @Delete("DELETE FROM `Book` WHERE `book_id` = #{book_id}")
+    void deleteById(Integer book_id);
 
 }
