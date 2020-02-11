26dc9bf1ac54d624bbbc8a543200a328deac957f
Eimantas Tarasevičius <eimataras@gmail.com>
2020-02-11 15:02:20 +0200

    Enabled database-backed authentication

--- src/main/java/lt/eimantas/eshop/security/MyUserPrincipal.java
+++ src/main/java/lt/eimantas/eshop/security/MyUserPrincipal.java
@@ -1,53 +1,62 @@
-//package lt.eimantas.eshop.security;
-//
-//import lombok.Data;
-//import lt.eimantas.eshop.model.User;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//
-//import java.util.Collection;
-//
-//@Data
-//public class MyUserPrincipal implements UserDetails {
-//
-//    private User user;
-//
-//    public MyUserPrincipal(User user) {
-//        this.user = user;
-//    }
-//
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return null;
-//    }
-//
-//    @Override
-//    public String getPassword() {
-//        return null;
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//        return null;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return false;
-//    }
-//}
+package lt.eimantas.eshop.security;
+
+import lt.eimantas.eshop.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class MyUserPrincipal implements UserDetails {
+
+    private final User user;
+
+
+    public MyUserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    // {noop}password pavercia String password suprantamu uzkoduotam DelegatingPasswordEncoder
+    @Override
+    public String getPassword() {
+        String result = "{noop}";
+        return result+user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        final List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("User"));
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
