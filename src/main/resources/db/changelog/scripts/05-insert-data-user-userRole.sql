42bf36d375ff97790abb1444ed882a50e5174807
Eimantas Tarasevičius <eimataras@gmail.com>
2020-06-23 12:29:30 +0300

    liquibase set up to seed a admin user

---
+++ src/main/resources/db/changelog/scripts/05-insert-data-user-userRole.sql
@@ -0,0 +1,3 @@
+insert into User(name, surname, username, password, uid) values ('Eimantas', 'Tarasevicius', 'eima@gmail.com', '$2y$12$0GdKt9qEZzhz27UudZcDCeoDpaEjj1k95gP2abNF.YPX/pZ7SgUFa
+', 'LQh4OD93gGdsL2p5n6GIGbfHKSh2');
+insert into UserRole(userId, roleId) values (1, 1);
--- src/main/resources/db/changelog/scripts/05-insert-data-user.sql
+++
@@ -1,1 +0,0 @@
---insert into User(name, surname, username, password, uid) values ('Eimantas', 'Tarasevicius', 'eima@gmail.com', 'eima123', 15);
